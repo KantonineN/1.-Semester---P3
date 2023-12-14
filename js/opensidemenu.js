3e62102ba3fc0c4badc22eefdafd8ff16173540c
KantonineN <nbg.100382@gmail.com>
2023-12-14 01:11:50 +0100

    projekter done

--- js/opensidemenu.js
+++ js/opensidemenu.js
@@ -1,9 +1,8 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
     document.getElementById("togglemenu").style.width = "50%";
-  }
-  
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("togglemenu").style.width = "0";
-  }
+}
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("togglemenu").style.width = "0";
+}
