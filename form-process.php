960a2ba3cbfa2b9f737da4495ff8b0c2d4ac34eb
KantonineN <nbg.100382@gmail.com>
2023-12-15 19:10:12 +0100

    15/12-23

---
+++ form-process.php
@@ -0,0 +1,78 @@
+<?php
+if (isset($_POST['email'])) {
+
+    // REPLACE THIS 2 LINES AS YOU DESIRE
+    $email_to = "kontakt@nikkag.dk";
+    $email_subject = "Ny henvendelse på din portfolio";
+
+    function problem($error)
+    {
+        echo "Oh looks like there is some problem with your form data: <br><br>";
+        echo $error . "<br><br>";
+        echo "Please fix those to proceed.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['name']) ||
+        !isset($_POST['firma']) ||
+        !isset($_POST['tel']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])
+    ) {
+        problem('Oh looks like there is some problem with your form data.');
+    }
+
+    $name = $_POST['name']; // required
+    $tel = $_TEL['tel'];
+    $email = $_POST['email']; // required
+    $message = $_POST['message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'Email address does not seem valid.<br>';
+    }
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'Name does not seem valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'Message should not be less than 2 characters<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details following:\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Firma: " . clean_string($firma) . "\n";
+    $email_message .= "Telefon: " . clean_string($tel) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- Replace this as your success message -->
+
+    Thanks for contacting us, we will get back to you as soon as possible.
+
+<?php
+}
+?>
